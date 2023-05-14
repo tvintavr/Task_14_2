111dff3038ce3b1f1406b91b7f66deffb61980a2
tvintavr <yuldashev.azat1998@list.ru>
2023-05-14 20:55:47 +0500

    modified:   Untitled-1.cpp 	modified:   Untitled-1.exe

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -60,11 +60,15 @@ int main() {
 
 
         // output table
+        std::cout<<"_______";
         std::cout<<std::endl;
         for (int i=0;i<3;i++) {
             for (int j=0;j<3;j++) {
-                std::cout<<cell[i][j];
+                std::cout<<"|"<<cell[i][j];
             }
+            std::cout<<"|";
+            std::cout<<std::endl;
+            std::cout<<"_______";
             std::cout<<std::endl;
         }
 
